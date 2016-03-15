8a4c9c2eae247783093f31970d3b752c8a327547
Krystan Menne <kmenne@gmail.com>
2016-03-15 09:15:06 -0700

    validate number

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,4 @@
 class Chapter < ActiveRecord::Base
   validates :name, presence: true
+  validates :number, presence: true
 end
--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Chapter, type: :model do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :number }
 end
