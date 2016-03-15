2f5a0e20d8d37274f8f6752f843b6707be4b1fe6
Krystan Menne <kmenne@gmail.com>
2016-03-15 15:11:43 -0700

    add method to return next lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,8 @@
 class Lesson < ActiveRecord::Base
   belongs_to :section
   validates :name, presence: true
+
+  def next
+    Lesson.where('id > ?', id).first
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,4 +3,22 @@ require 'rails_helper'
 RSpec.describe Lesson, type: :model do
   it { should validate_presence_of :name }
   it { should belong_to :section }
+
+  context '#next' do
+    it 'returns the lesson with the next-higher number /
+      than the current lesson' do
+      section = Section.create(name: 'Test Section')
+      other_section = Section.create(name: 'Other Section')
+      current_lesson = Lesson.create(name: 'lesson1',
+                                     number: 1,
+                                     section: section)
+      next_lesson = Lesson.create(name: 'lesson2',
+                                  number: 3,
+                                  section: section)
+      Lesson.create(name: 'other lesson',
+                    number: 2,
+                    section: other_section)
+      expect(current_lesson.next).to eq next_lesson
+    end
+  end
 end
