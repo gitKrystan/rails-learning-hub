007d0e322acdc666166d33bec43eb59180f06aa8
Krystan Menne <kmenne@gmail.com>
2016-03-15 14:54:54 -0700

    remove section/lesson route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root to: 'chapters#index'
   resources :lessons
-  resources :sections do
-    resources :lessons, only: [:show]
-  end
+  resources :sections
   resources :chapters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
