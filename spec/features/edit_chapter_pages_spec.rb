cccd8954fad9842e94575f71b63a1960c23e5557
Krystan Menne <kmenne@gmail.com>
2016-03-15 10:58:30 -0700

    add test for chapter errors

--- spec/features/edit_chapter_pages_spec.rb
+++ spec/features/edit_chapter_pages_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 describe 'the edit a chapter process' do
-  it 'edits a chapter' do
+  before do
     chapter = Chapter.create(name: 'Test Chapter')
     visit chapter_path(chapter)
     click_on 'Edit'
+  end
+
+  it 'edits a chapter' do
     fill_in 'Name', with: 'New Chapter Name'
     click_on 'Update Chapter'
     expect(page).to have_content 'New Chapter Name'
+  end
+
+  it 'gives error when no name is entered' do
+    fill_in 'Name', with: ''
+    click_on 'Update Chapter'
+    expect(page).to have_content 'error'
   end
 end
