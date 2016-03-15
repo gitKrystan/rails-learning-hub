e697ceff46773ed14398d03c32181afda0b102f3
Krystan Menne <kmenne@gmail.com>
2016-03-15 15:23:01 -0700

    add method to determine next lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,8 @@ class Lesson < ActiveRecord::Base
   def next
     Lesson.where('number > ? AND section_id = ?', number, section.id).first
   end
+
+  def previous
+    Lesson.where('number < ? AND section_id = ?', number, section.id).first
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -12,13 +12,31 @@ RSpec.describe Lesson, type: :model do
       current_lesson = Lesson.create(name: 'lesson1',
                                      number: 1,
                                      section: section)
-      other_lesson = Lesson.create(name: 'other lesson',
-                     number: 2,
-                     section: other_section)
+      Lesson.create(name: 'other lesson',
+                    number: 2,
+                    section: other_section)
       next_lesson = Lesson.create(name: 'lesson2',
                                   number: 3,
                                   section: section)
       expect(current_lesson.next).to eq next_lesson
+    end
+  end
+
+  context '#previous' do
+    it 'returns the lesson with the next-lower number /
+      than the current lesson' do
+      section = Section.create(name: 'Test Section')
+      other_section = Section.create(name: 'Other Section')
+      current_lesson = Lesson.create(name: 'lesson1',
+                                     number: 1,
+                                     section: section)
+      Lesson.create(name: 'other lesson',
+                    number: 2,
+                    section: other_section)
+      next_lesson = Lesson.create(name: 'lesson2',
+                                  number: 3,
+                                  section: section)
+      expect(next_lesson.previous).to eq current_lesson
     end
   end
 end
